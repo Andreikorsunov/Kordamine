93f8108674b5a1fb330ebe9e50e9d0ce556f12e8
AndreiKoršunov <andrei.korshunov2004@gmail.com>
2021-10-13 11:22:12 +0300

    abivahend

---
+++ Abivahend.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Korsunovulesanded
+{
+    class Abivahend
+    {
+        public static void Main(string[] arg)
+        {
+            Console.WriteLine(Alamprogramm.Korruta(3, 6));
+            Double arv1 = 5;
+            Double arv2 = 6;
+
+            Double resultik = Alamprogramm.Eval(arv1.ToString() + "+" + arv2.ToString());
+            Console.WriteLine(resultik);
+            Console.ReadLine();
+        }
+    }
+}
